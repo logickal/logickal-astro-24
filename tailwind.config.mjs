2dad0b8a03025d7c168867896d6e9ddde4be9641
Jeremy Dickens <jeremy.dickens@pantheon.io>
2024-07-22 07:56:35 -0500

    Add Tailwind typography plugin, set up initial prose component, set up initial colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.neutral,
+			red: colors.red,
+			yellow: colors.amber,
+			green: colors.green,
+			blue: colors.blue,
+			indigo: colors.indigo,
+			purple: colors.purple,
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography')
+	],
 }
