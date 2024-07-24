ef5a11af4785e8202726c3e6d5db8638795b54ac
Jeremy Dickens <jeremy.dickens@pantheon.io>
2024-07-24 12:46:12 -0500

    Trying to build the discography while adding links from google results

--- buildDiscog.js
+++ buildDiscog.js
@@ -2,6 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import Papa from 'papaparse';
 import { parse } from 'date-fns';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
+
 
 let discog = [];
 
@@ -28,12 +32,13 @@ function initialBuild() {
             label: album.label,
             labelUrl: album.labelUrl
         };
-
+        console.log(`Album: ${albumInfo.title} - Initial data run`);
         discog.push(albumInfo);
     });
 
     // Grab additional info from the JSON data:
     discog.forEach((album, index) => {
+        console.log(`Updating album: ${album.title}`);
         // Find the matching album in the bandcamp data
         let match = bandcampData.find(bandcampAlbum => bandcampAlbum.title === album.title);
         // We specifically want to populate the tags, id, credits, and about fields
@@ -46,13 +51,89 @@ function initialBuild() {
             if (!album.bandcampUrl) {
                 album.bandcampUrl = match.url;
             }
+
+            // If the album is missing Apple/Spotify/Beatport URLs, search for them using the title
+            if (!album.appleMusicUrl) {
+                console.log(`Searching for Apple Music URL for ${album.title}`);
+                album.appleMusicUrl = retrieveServiceUrl(album.title, 'apple', album.slug);
+            }
+            if (!album.spotifyUrl) {
+                console.log(`Searching for Spotify URL for ${album.title}`);
+                album.spotifyUrl = retrieveServiceUrl(album.title, 'spotify', album.slug);
+            }
+            if (!album.beatportUrl) {
+                console.log(`Searching for Beatport URL for ${album.title}`);
+                album.beatportUrl = retrieveServiceUrl(album.title, 'beatport', album.slug);
+            }
             //write this entry back to the discog array
+            console.log(`Updating album: ${album.title} - Final data run`);
             discog[index] = album;
         }
 
     });
 
+
 }
 
+async function retrieveServiceUrl(title, service, slug) {
+    const searchString = `${title} logickal`;
+    let searchSite = null;
+    let albumDesignator = null;
+    switch (service) {
+        case 'spotify':
+            searchSite = `open.spotify.com`;
+            albumDesignator = 'album';
+            break;
+        case 'apple':
+            searchSite = `music.apple.com`;
+            albumDesignator = 'album';
+            break;
+        case 'beatport':
+            searchSite =  `beatport.com`;
+            albumDesignator = 'release';
+            break;
+        default:
+            throw new Error('Invalid service');
+    }
+
+    let searchQuery = `site:${searchSite} ${searchString}`;
+
+
+    let searchUrl = `https://www.google.com/search?q=${searchQuery}`;
+
+   try {
+    // retrieve the array of links from the google search results
+    const data = await fetchGoogleResults(searchUrl);
+
+    // Look for the link in the array that contains both the searchSite and the albumDesignator
+    // console.log(`Title: ${title}`);
+    const serviceLink = data.find(link => link.includes(searchSite) && link.includes(albumDesignator));
+
+    // console.log(`ServiceLink: ${serviceLink}`);
+
+   }
+   catch (err) {
+       console.error(err);
+   }
+
+
+}
+async function fetchGoogleResults(searchUrl) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.goto(searchUrl, {
+        waitUntil: 'networkidle2', // Wait until the network is idle
+        timeout: 60000 // Increase timeout to 60 seconds
+    });
+
+    const results = await page.content();
+    const hrefs = await page.$$eval('a', as => as.map(a => a.href));
+    await browser.close();
+
+    return hrefs;
+}
+
+//retrieveServiceUrl('Speaker Worship', 'apple', 'speaker-worship');
 initialBuild();
 fs.writeFileSync(path.join(import.meta.dirname, 'src/assets/slimDiscog.json'), JSON.stringify(discog, null, 2));
