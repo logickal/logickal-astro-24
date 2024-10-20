334b6038edca4fe3d4f0a119e9257ce900f6b1e2
jeremydickens <jeremy@offnominal.com>
2024-10-20 11:18:02 -0500

    Working on streaming icons for releases

--- src/assets/lib/servicesData.js
+++ src/assets/lib/servicesData.js
@@ -1,15 +1,17 @@
-let ServiceIcons = [
+let ServicesData = [
     {
       name: "Bandcamp",
       icon: "icons8-bandcamp.svg",
       url: "https://logickal.bandcamp.com",
       type: 'music',
+      dataName: 'bandcamp'
     },
     {
       name: "Apple Music",
       icon: "icons8-apple-music.svg",
       url: "https://music.apple.com/us/artist/logickal/156855379",
       type: 'music',
+      dataName: 'appleMusic'
 
     },
     {
@@ -17,6 +19,7 @@ let ServiceIcons = [
       icon: "icons8-spotify.svg",
       url: "https://open.spotify.com/artist/6Mvi6AM1TSZGr0vWqV6GNM?si=t_UaaeHNR_6I-OgxuT1yyw",
       type: 'music',
+      dataName: 'spotify'
 
     },
     {
@@ -24,6 +27,7 @@ let ServiceIcons = [
       icon: "icons8-amazon-music.svg",
       url: "https://amazon.com/music/player/artists/B000RSE9GG/logickal?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_jyGEzdmqx4DYnTe0BrjfcYVlj",
       type: 'music',
+      dataName: 'amazonMusic'
 
     },
     {
@@ -31,6 +35,7 @@ let ServiceIcons = [
       icon: "icons8-youtube-music.svg",
       url: "https://music.youtube.com/channel/UCtewbGbrBYnVVfyQcn89GeQ?si=JaejM18j-DzRTTYk",
       type: 'music',
+      dataName: 'youtubeMusic'
 
     },
     {
@@ -38,6 +43,7 @@ let ServiceIcons = [
       icon: "icons8-tidal.svg",
       url: "https://www.tidal.com",
       type: 'music',
+      dataName: 'tidal'
 
     },
     {
@@ -45,6 +51,7 @@ let ServiceIcons = [
       icon: "icons8-soundcloud.svg",
       url: "https://www.soundcloud.com",
       type: 'music',
+      dataName: 'soundcloud'
 
     },
     {
@@ -52,19 +59,36 @@ let ServiceIcons = [
         icon: "icons8-facebook.svg",
         url: "https://www.facebook.com/logickal",
         type: 'social',
+        dataName: 'facebook'
     },
     {
         name: "Instagram",
         icon: "icons8-instagram.svg",
         url: "https://www.instagram.com/logickal",
         type: 'social',
+        dataName: 'instagram'
     },
     {
         name: 'Mastodon',
         icon: 'icons8-mastodon.svg',
         url: 'https://mastodon.social/@logickal',
         type: 'social',
+        dataName: 'mastodon'
+    }, 
+    {
+        name: "YouTube",
+        icon: "icons8-youtube.svg",
+        url: "https://www.youtube.com/channel/UCtewbGbrBYnVVfyQcn89GeQ",
+        type: 'social',
+        dataName: 'youtube'
+    },
+    {
+        name: "Twitch",
+        icon: "icons8-twitch.svg",
+        url: "https://www.twitch.tv/logickal_musick",
+        type: 'social',
+        dataName: 'twitch'
     }
   ];
   
-  export default ServiceIcons;
+  export default ServicesData;
