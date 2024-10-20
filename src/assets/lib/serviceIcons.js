bd042ef365b53068bd3973de321b515d60dabb2d
jeremydickens <jeremy@offnominal.com>
2024-10-20 10:38:41 -0500

    Putting the service icons array into the lib to abstract it away.

---
+++ src/assets/lib/serviceIcons.js
@@ -0,0 +1,70 @@
+let ServiceIcons = [
+    {
+      name: "Bandcamp",
+      icon: "icons8-bandcamp.svg",
+      url: "https://logickal.bandcamp.com",
+      type: 'music',
+    },
+    {
+      name: "Apple Music",
+      icon: "icons8-apple-music.svg",
+      url: "https://music.apple.com/us/artist/logickal/156855379",
+      type: 'music',
+
+    },
+    {
+      name: "Spotify",
+      icon: "icons8-spotify.svg",
+      url: "https://open.spotify.com/artist/6Mvi6AM1TSZGr0vWqV6GNM?si=t_UaaeHNR_6I-OgxuT1yyw",
+      type: 'music',
+
+    },
+    {
+      name: "Amazon Music",
+      icon: "icons8-amazon-music.svg",
+      url: "https://amazon.com/music/player/artists/B000RSE9GG/logickal?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_jyGEzdmqx4DYnTe0BrjfcYVlj",
+      type: 'music',
+
+    },
+    {
+      name: "YouTube Music",
+      icon: "icons8-youtube-music.svg",
+      url: "https://music.youtube.com/channel/UCtewbGbrBYnVVfyQcn89GeQ?si=JaejM18j-DzRTTYk",
+      type: 'music',
+
+    },
+    {
+      name: "Tidal",
+      icon: "icons8-tidal.svg",
+      url: "https://www.tidal.com",
+      type: 'music',
+
+    },
+    {
+      name: "Soundcloud",
+      icon: "icons8-soundcloud.svg",
+      url: "https://www.soundcloud.com",
+      type: 'music',
+
+    },
+    {
+        name: "Facebook",
+        icon: "icons8-facebook.svg",
+        url: "https://www.facebook.com/logickal",
+        type: 'social',
+    },
+    {
+        name: "Instagram",
+        icon: "icons8-instagram.svg",
+        url: "https://www.instagram.com/logickal",
+        type: 'social',
+    },
+    {
+        name: 'Mastodon',
+        icon: 'icons8-mastodon.svg',
+        url: 'https://mastodon.social/@logickal',
+        type: 'social',
+    }
+  ];
+  
+  export default ServiceIcons;
