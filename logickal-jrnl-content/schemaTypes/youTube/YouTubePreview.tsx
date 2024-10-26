2e2943c0c61c085ca25323dbc160c363b6672c9a
jeremydickens <jeremy@offnominal.com>
2024-10-26 09:56:44 -0500

    Fixing preview?

--- logickal-jrnl-content/schemaTypes/youTube/YouTubePreview.tsx
+++ logickal-jrnl-content/schemaTypes/youTube/YouTubePreview.tsx
@@ -1,18 +1,25 @@
 // ./src/schemas/schemaTypes/youTubeType/YouTubePreview.tsx
 
 import type {PreviewProps} from 'sanity'
-import {Flex, Text} from '@sanity/ui'
+import {Flex, Text, ThemeProvider, studioTheme} from '@sanity/ui'
+import React from 'react'
 import YouTubePlayer from 'react-player/youtube'
 
 export function YouTubePreview(props: PreviewProps) {
-    console.log('Props: ', props)
-  const {title: url} = props
+  console.log('Props: ', props)
+  const {title: url} = props || {}
+
+  console.log('URL: ', url);
 
   return (
-    <Flex padding={3} align="center" justify="center">
-      {typeof url === 'string' 
-        ? <YouTubePlayer url={url} /> 
-        : <Text>Add a YouTube URL</Text>}
-    </Flex>
+    <ThemeProvider theme={studioTheme}>
+      <Flex padding={3} align="center" justify="center">
+        {typeof url === 'string' && url ? (
+          <YouTubePlayer url={url} />
+        ) : (
+          <Text>Add a YouTube URL</Text>
+        )}
+      </Flex>
+    </ThemeProvider>
   )
 }
